b52ecb810a4d7d09d625467b638e87bf11806cc7
Leonardo Costa <leonardocostapsi@gmail.com>
2023-02-18 15:43:19 -0300

    Documenta rota POST /users, GET /users, GET /me e POST /session

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -8,6 +8,5 @@ export const usersRoutes = Router();
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
 
-usersRoutes.use(ensureAuthenticated);
 usersRoutes.post("/", createUserController.handle);
-usersRoutes.get("/", listUsersController.handle);
+usersRoutes.get("/", ensureAuthenticated, listUsersController.handle);
