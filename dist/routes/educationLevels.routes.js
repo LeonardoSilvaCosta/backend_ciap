bd62370e6db565a538ff41b702c6c838b2ab3c4e
Leonardo Costa <leonardocostapsi@gmail.com>
2023-02-21 13:22:09 -0300

    troca script start

--- dist/routes/educationLevels.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.educationLevelsRoutes = void 0;
-const express_1 = require("express");
-const CreateEducationLevelController_1 = require("../modules/accounts/useCases/createEducationLevel/CreateEducationLevelController");
-exports.educationLevelsRoutes = (0, express_1.Router)();
-const educationLevelController = new CreateEducationLevelController_1.CreateEducationLevelController();
-exports.educationLevelsRoutes.post("/", educationLevelController.handle);
